dcfbc1fb23c572b700da7026a0a7f02a2b4b4bab
juliavillela <33360385+juliavillela@users.noreply.github.com>
2020-08-05 22:10:03 -0300

    formats form elements

---
+++ js/form.js
@@ -0,0 +1,6 @@
+function save_message(){
+    //#TBD
+    let email = document.getElementById("email");
+    let subject = document.getElementById("subject");
+    let message = document.getElementById("message");
+}
