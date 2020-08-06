c98d0de1bce194ebdcde719928ce8aaad33f7b4b
juliavillela <33360385+juliavillela@users.noreply.github.com>
2020-08-06 18:51:49 -0300

    adds close button to details

--- js/details_gallery.js
+++ js/details_gallery.js
@@ -1,26 +1,24 @@
-var project = document.getElementById("selected_test");
+// var project = document.getElementById("selected");
+// document.getElementById("selected_test");
 var visible_image = 0;
 
 function select(element) {
     let this_project = element.parentNode;
+    let previous = document.getElementById("selected");
     if (this_project.id === "selected") {
         return 0;
     }
-    let previous = document.getElementById("selected");
     if (previous) {
-        close(previous);
+        close_detail()
     }
-    this_project.id ="selected";
+    this_project.id = "selected";
     this_project.scrollIntoView();
 }
 
-function close(previous) {
-    let prev_children = previous.children;
-    previous.id = "unset";
-    previous.open = false;
-    // let children = previous.children;
-    // children[0].className = "square-image-container";
-    // children[0].children[0].className = "overlay";
+function close_detail() {
+    let project = document.getElementById("selected");
+    project.id = "unset";
+    project.open = false;
 }
 
 
